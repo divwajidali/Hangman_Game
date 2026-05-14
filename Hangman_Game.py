5eac16f3517e2b50efeba086462aa7be85216237
divwajidali <dev.wajidali@gmail.com>
2026-05-14 11:35:32 +0500

    Add Words

---
+++ Hangman_Game.py
@@ -0,0 +1,86 @@
+easy_words = [
+    "apple",
+    "table",
+    "chair",
+    "bread",
+    "plant",
+    "phone",
+    "mouse",
+    "water",
+    "pizza",
+    "happy"
+]
+medium_words = [
+    "python",
+    "laptop",
+    "science",
+    "diamond",
+    "picture",
+    "country",
+    "teacher",
+    "monster",
+    "battery",
+    "holiday"
+]
+hard_words = [
+    "algorithm",
+    "developer",
+    "artificial",
+    "cybersecurity",
+    "programming",
+    "javascript",
+    "engineering",
+    "microprocessor",
+    "communication",
+    "transformation"
+]
+animals = [
+    "tiger",
+    "elephant",
+    "giraffe",
+    "kangaroo",
+    "dolphin",
+    "penguin",
+    "alligator",
+    "rabbit",
+    "monkey",
+    "zebra"
+]
+countries = [
+    "pakistan",
+    "canada",
+    "brazil",
+    "germany",
+    "japan",
+    "turkey",
+    "france",
+    "italy",
+    "china",
+    "india"
+]
+technology = [
+    "python",
+    "computer",
+    "keyboard",
+    "internet",
+    "database",
+    "software",
+    "hardware",
+    "network",
+    "coding",
+    "developer"
+]
+fun_words = [
+    "banana",
+    "popcorn",
+    "chocolate",
+    "burger",
+    "toaster",
+    "robot",
+    "dragon",
+    "ninja",
+    "zombie",
+    "pirate"
+]
+
+
