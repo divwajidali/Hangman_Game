3ddd52562f3e403b8701767b69335c12ec0883a0
divwajidali <dev.wajidali@gmail.com>
2026-05-14 12:03:21 +0500

    Update Code

--- Hangman_Game.py
+++ Hangman_Game.py
@@ -93,8 +93,29 @@ word_categories = {
     6 : technology,
     7 : fun_words
 }
+while True:
 
-choice = int(input("1. Easy Words\n2. Medium Words\n3. Hard Words\n4. Animals\n5. Countries\n6. Technologies\n7. Fun Words\nEnter Choice :"))
+    choice = input("1. Easy Words\n2. Medium Words\n3. Hard Words\n4. Animals\n5. Countries\n6. Technologies\n7. Fun Words\nEnter Choice :")
+    try:
+        choice = int(choice)
+        if choice in range(1, 8):
+            selected_words = word_categories[choice]
+            break
 
-selected_words = word_categories[choice]
-print(selected_words)
+        else:
+            print("You entered an invalid option.\nPlease enter valid choice.")
+
+
+
+    except ValueError:
+        print("You entered an invalid option.\nPlease enter valid choice.")
+    
+
+    
+
+
+    
+import random
+
+target = random.choice(selected_words)
+
